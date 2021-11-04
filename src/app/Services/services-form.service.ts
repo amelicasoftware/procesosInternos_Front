165f5e6cf8b9681ac65ed907cf764fa47a365f42
redamaidy <againstedgar@gmail.com>
2021-11-04 17:10:40 -0600

    servicio

--- src/app/Services/services-form.service.ts
+++ src/app/Services/services-form.service.ts
@@ -23,19 +23,9 @@ export class ServicesFormService {
 
   postDatos(form: any): Observable<any>{
     delete form.value.listAutor;
-<<<<<<< HEAD
-    console.log(`http://148.215.24.22:9084/boardInvestigacion/service/ds/sendRegisterOfProy/${JSON.stringify(form.value)}`);
-    // return this.http.get(`http://148.215.24.22:9084/boardInvestigacion/service/ds/sendRegisterOfProy/${JSON.stringify(form.value)}`);
-    return this.http.post(`http://148.215.24.22:9084/boardInvestigacion/service/ds/sendRegisterOfProy/${JSON.stringify(form.value)}`, (mensaje: String) =>{
-      console.log(mensaje);
-    });
-  }
-}
-=======
     console.log(`${this.urlService}sendRegisterOfProy/${JSON.stringify(form.value)}`);
     return this.http.post(`${this.urlService}sendRegisterOfProy/${JSON.stringify(form.value)}`, (mensaje: String) =>{
       console.log(mensaje);
     });
   }
 }
->>>>>>> 89df7ed37dd7209b7bc4462c04fc05e053789997
