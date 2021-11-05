9a23f9f11bc88f3daa69a3d31c307bb800a709ee
redamaidy <againstedgar@gmail.com>
2021-11-05 17:39:16 -0600

    formato de fecha y función cancelar

--- src/app/components/articulos-de-divulgacion/articulos-de-divulgacion.component.ts
+++ src/app/components/articulos-de-divulgacion/articulos-de-divulgacion.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, NgModule } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ServicesFormService } from 'src/app/Services/services-form.service';
 import Swal from 'sweetalert2';
+import * as moment from 'moment';
 @Component({
   selector: 'app-articulos-de-divulgacion',
   templateUrl: './articulos-de-divulgacion.component.html',
@@ -16,7 +17,7 @@ export class ArticulosDeDivulgacionComponent implements OnInit {
   autores: String[] = [];
   lista: any[] = [];
   dato: boolean = true;
-
+  selectedCountry:any=[];
   constructor(
     private servicesForm: ServicesFormService,
     private fb: FormBuilder
@@ -58,7 +59,7 @@ export class ArticulosDeDivulgacionComponent implements OnInit {
       ALCPROYINV: new FormControl('', [Validators.required]),
       PRDPROYINV: new FormControl(''),
       MESPROYINV: new FormControl(''),
-      FECCAPPROY: new FormControl(''),
+      FECCAPPROY: new FormControl(this.fechaActual()),
       REAPROYINV: new FormControl('', [Validators.required]),
       AGDREDPROY: new FormControl('', [Validators.required]),
       TPOACTPROY: new FormControl(''),
@@ -118,6 +119,7 @@ export class ArticulosDeDivulgacionComponent implements OnInit {
     this.servicesForm.postDatos(this.form).subscribe(mensaje => {
       console.log(mensaje);
       if(mensaje.respuesta){
+        this.limpiar();
         this.alertWithSuccess();
       }else{
         this.erroalert();
@@ -145,5 +147,10 @@ export class ArticulosDeDivulgacionComponent implements OnInit {
   limpiar(){
     this.autoresArr.clear();
     this.form.reset();
+    this.selectedCountry = [];
+  }
+  fechaActual(): String{
+    let fecha = new Date;
+    return moment(fecha).format('DD-MM-YY');
   }
 }
