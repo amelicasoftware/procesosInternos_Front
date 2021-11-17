9233410c898b652cd5fab453c46f1806c575975b
jesus chapulin gonzalez <chaps.jesus.gz@gmail.com>
2021-11-17 16:31:14 -0600

    se hacen cambios para mejorar los filtros de busqueda

--- src/app/Services/busquedas.service.ts
+++ src/app/Services/busquedas.service.ts
@@ -35,35 +35,35 @@ var dataBusqueda=JSON.parse(str);
     return this.http.get<any>(url).toPromise()
   }
   sendInfo() { 
-    var jsonInsert='{"TITPROYINV":"relaciones nuevas",'+
-        '"TPOPROYINV":"Capitulo",'+
-        '"ANIOPROYINV":2021,'+
+    var jsonInsert='{"TITPROYINV":"Articulo angular de prueba ANG",'+
+        '"TPOPROYINV":"Artículos científicos",'+
+        '"ANIOPROYINV":1999,'+
         '"MESPROYINV":"Octubre",'+
         '"RSMPROYINV":"en este articulo se abordan distintas formas de hacer politica con un enfoque responsable y practico",'+
         '"AUTPROYINV":"jesus",'+
-        '"URLPROYINV":"articulo.oa",'+
+        '"URLPROYINV":"https:s-ss-swww.redalyc.orgs-shome.oad-did=20",'+
         '"VOLPROYINV":"01",'+
         '"FTEPROYINV":"Libros",'+
         '"INSPROYINV":"UAEM",'+
         '"AUTPADPROY":"jesus",'+
         '"PARPROYINV":"ninguna",'+
         '"CTDINTPROY":"1",'+
-        '"ALCPROYINV":"nacional",'+
+        '"ALCPROYINV":"Nacional",'+
         '"PRDPROYINV":"Marzo - Agosto",'+
-        '"FECCAPPROY":"03-11-21",'+
+        '"FECCAPPROY":"17-11-21",'+
         '"REAPROYINV":"1",'+
         '"AGDREDPROY":"si",'+
         '"TPOACTPROY":"comunicación",'+
         '"INFADCPROY":"Este articulo es una prueba",'+
-        '"CVEPAISPRO":"73,30,10,5"'+
+        '"CVEPAISPRO":"5"'+
         '}';
         var url='http://148.215.24.22:9084/boardInvestigacion/service/ds/sendRegisterOfProy/'+jsonInsert
     return this.http.post<any>(url,"").toPromise()
   }
   editInfo() { 
-    var jsonUpdate='{"CVEPROYINV":"84",'+
+    var jsonUpdate='{"CVEPROYINV":"34",'+
         '"TITPROYINV":"política social en america latina",'+
-        '"TPOPROYINV":"Nota periodistica",'+
+        '"TPOPROYINV":"Artículos científicos",'+
         '"ANIOPROYINV":2019,'+
         '"MESPROYINV":"Octubre",'+
         '"RSMPROYINV":"en este articulo se abordan distintas formas de hacer politica con un enfoque responsable y practico",'+
@@ -75,19 +75,29 @@ var dataBusqueda=JSON.parse(str);
         '"AUTPADPROY":"jesus",'+
         '"PARPROYINV":"ninguna",'+
         '"CTDINTPROY":"1",'+
-        '"ALCPROYINV":"nacional",'+
+        '"ALCPROYINV":"Nacional",'+
         '"PRDPROYINV":"Marzo - Agosto",'+
-        '"FECCAPPROY":"03-11-21",'+
+        '"FECCAPPROY":"17-11-21",'+
         '"REAPROYINV":"1",'+
         '"AGDREDPROY":"si",'+
         '"TPOACTPROY":"comunicación",'+
         '"INFADCPROY":"Este articulo es una prueba",'+
-        '"CVEPAISPRO":"73"'+
+        '"CVEPAISPRO":"73,5"'+
         '}';
         var url='http://148.215.24.22:9084/boardInvestigacion/service/ds/sendUpdateOfProy/'+jsonUpdate
     return this.http.post<any>(url,"").toPromise()
   }
-  getFilters(){
-    return this.http.get<any>('http://148.215.24.22:9084/boardInvestigacion/service/ds/getDataProyFilters').toPromise()
+  getFilters(data:any){
+    var str=JSON.stringify(data);
+    var dataBusqueda=JSON.parse(str);
+    var json=   "{\"TITPROYINV\":\""+dataBusqueda.TITPROYINV+"\","+
+        "\"TPOPROYINV\":\""+dataBusqueda.TPOPROYINV+"\","+
+        "\"ANIOPROYINV\":\""+dataBusqueda.ANIOPROYINV+"\","+
+        "\"MESPROYINV\":\""+dataBusqueda.MESPROYINV+"\","+
+        "\"ALCPROYINV\":\""+dataBusqueda.ALCPROYINV+"\","+
+        "\"PERIIN\":\""+dataBusqueda.PERIIN+"\","+
+        "\"PERIFIN\":\""+dataBusqueda.PERIFIN+"\""+
+      "}";
+    return this.http.get<any>('http://148.215.24.22:9084/boardInvestigacion/service/ds/getDataProyFilters/'+json).toPromise()
   }
 }
