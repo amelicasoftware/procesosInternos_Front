5a45306da86b404fefb2c82f072a6a5d0100d6d1
langelesp <angel_apineda@hotmail.com>
2022-01-25 12:02:28 -0600

    ruta producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   urlServices: 'http://148.215.2.10:9092/boardInvestigacion/service/ds/',
-  url: 'http://127.0.0.1:4200/#/'
+  url: 'http://148.215.2.10:9092/boardInvestigacion#/'
 };
